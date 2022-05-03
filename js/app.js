e3d54cf388ad6fa951ad89befc476fc50dfca8f2
Tim Crisp <timothycrisp@gmail.com>
2022-05-03 18:25:40 -0500

    declare variable and cached element references

--- js/app.js
+++ js/app.js
@@ -3,10 +3,21 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let squares = []
+let turn
+// isWinner: a player that won, a tie, game is still in play
+let isWinner
 
 /*------------------------ Cached Element References ------------------------*/
+const sq0 = document.querySelector('#sq0')
+const sq1 = document.querySelector('#sq1')
+const sq2 = document.querySelector('#sq2')
+const sq3 = document.querySelector('#sq3')
+const sq4 = document.querySelector('#sq4')
+const sq5 = document.querySelector('#sq5')
+const sq6 = document.querySelector('#sq6')
+const sq7 = document.querySelector('#sq7')
+const sq8 = document.querySelector('#sq8')
 
 
 
