993a8a5a14b0884f17239ace95249497712f025c
Tim Crisp <timothycrisp@gmail.com>
2022-05-04 06:06:59 -0500

    begin stub up render function

--- js/app.js
+++ js/app.js
@@ -60,16 +60,25 @@ function handleClick(evt) {
   turn *= -1
   console.log(turn)
   
-  // render()
+  render()
   // getWinner
 }
 
+// 3.3) The render function should
+	  // 3.3.1) Loop over the board array (which represents the squares on the page), and FOR EACH iteration:
+		  // 3.3.1.1) Use the INDEX of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
+		  // 3.3.1.2) Style that square however you wish dependant on the value contained in the current cell being iterated over (-1, 1, or null)
+	  // 3.3.2) Render a message reflecting the currrent game state:
+	    // 3.3.2.1) If winner has a value other than null (game still in progress), render whose turn it is.
+	      // Hint: Maybe use a ternary inside of a template literal here?
+	    // 3.3.2.2) If winner is equal to 'T' (tie), render a tie message.
+	    // 3.3.2.3) Otherwise, render a congratulatory message to which player has won.
+	      // Hint (again): Maybe use a ternary inside a template literal here
 
 function render() {
-  if (!isWinner) {
-    for (let i = 0; i < squares.length; i++) {
-      // set i value = player
-      console.log(squares[i])
+  squares.forEach(function(square, idx) {
+    if (square === 1) {
+      square[idx].textContent = 'X'
     }
-  }
+  })
 }
