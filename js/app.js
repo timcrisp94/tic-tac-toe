6fb123f4312eac5143db96648d6d9c19892f2f67
Tim Crisp <timothycrisp@gmail.com>
2022-05-15 00:21:59 -0400

    tie logic

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ const winningCombos = [
 ]
 
 let squares = []
-let boardArray, turn, isWinner
+let boardArray, turn, clicks, isWinner
 
+const title = document.querySelector('#title')
 const message = document.querySelector('#message')
 const board = document.querySelector('.board')
 const sq0 = document.querySelector('#sq0')
@@ -19,12 +20,14 @@ const sq6 = document.querySelector('#sq6')
 const sq7 = document.querySelector('#sq7')
 const sq8 = document.querySelector('#sq8')
 
+const resetBtn = document.getElementById('reset-button')
+// resetBtn.addEventListener('click', )
+
 board.addEventListener("click", handleClick)
 
 init();
 
 function init() {  
-  
   boardArray = [
     null, null, null,
     null, null, null,
@@ -37,9 +40,10 @@ function init() {
   ]
   isWinner = null
   turn = 1
-  message.textContent = 'X goes first'
-
-  render()
+  clicks = 0
+  console.log (clicks)
+  message.textContent = 'x goes first'
+  
 }
 
 function handleClick(evt) {
@@ -52,53 +56,57 @@ function handleClick(evt) {
   } 
   boardArray[idx] = turn
   turn *= -1
+  clicks++
+  console.log(clicks)
+
   render()
   getWinner()
+  
+  
 }
 
 function render() {
   for (let i = 0; i < boardArray.length && squares; i++) {
-
     if (boardArray[i] === 1) {
       squares[i].textContent = 'X'
-
     } else if (boardArray[i] === -1) {
       squares[i].textContent = 'O'
-
     }
   }
-
   if (!isWinner) {
-    
-    turn === 1 ? message.textContent = `it is X's turn` : 
-      message.textContent = `now it's O's turn`
-    } else if (isWinner === 'T') {
-      message.textContent = `How climactic! It's a tie!`
-    } else if (isWinner) {
-      isWinner === 1 ? message.textContent = `X Wins!` : 
-      message.textContent = `O Wins!`
-    } 
-    
+    turn === 1 ? message.textContent = `it is X's turn` : message.textContent = `now it's O's turn`
+  }    
 }
 
 function getWinner() {
   
-  winningCombos.forEach(function(winningCombo) { 
-    
-    if (boardArray[winningCombo[0]] + boardArray[winningCombo[1]] + boardArray[winningCombo[2]] === 3) {
-      isWinner = 1
-      message.textContent = 'X WINS!'
-    } else 
-    if (boardArray[winningCombo[0]] + boardArray[winningCombo[1]] + boardArray[winningCombo[2]] === -3) {
-      isWinner = -1
-      message.textContent = '0 WINS!'
-    } 
-  })
+  if (clicks < 9) {
+    winningCombos.forEach(function(winningCombo) {  
+      if (boardArray[winningCombo[0]] + boardArray[winningCombo[1]] + boardArray[winningCombo[2]] === 3) {
+        isWinner = 1
+        message.textContent = 'X WINS!'
+      } else 
+      if (boardArray[winningCombo[0]] + boardArray[winningCombo[1]] + boardArray[winningCombo[2]] === -3) {
+        isWinner = -1
+        message.textContent = '0 WINS!'
+      } 
+    })
+  } else {
+    message.textContent = 'oh, a tie'
+  }
 
 } 
 
-  
-  
 
 
-// console.log(getWinner())
+
+// let timeLeft = 2
+// setInterval(function() {
+//   // this way
+//   timeLeft -= 1
+//   if (timeLeft < 0) {
+//   // now this way
+//   }  
+// }, 1000)
+
+
